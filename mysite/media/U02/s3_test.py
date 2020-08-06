7c7207b46364be8ff6e77b7975ca333f1b6e307e
Samantha Chan <sam.chanWT@gmail.com>
2020-08-06 23:40:05 +1200

    reset migrations and database

--- mysite/media/U02/s3_test.py
+++
@@ -1,4 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3')
-s3.upload_file('M_3.mp3','djangomediakinvoice','M_3upload.mp3')
--- mysite/remindmeapp/models.py
+++ mysite/remindmeapp/models.py
@@ -5,6 +5,8 @@ class Reminder(models.Model):
 	pid = models.CharField(max_length=60)
 	txt = models.CharField(max_length=60)
 	indx = models.CharField(max_length=60)
+	remdate = models.CharField(max_length=60)
+	remtime = models.CharField(max_length=60)
 	date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
